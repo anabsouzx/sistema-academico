0b76d02628792dc7eeba3c494166a3579ab3bd63
Ana Beatriz <itzzzs2pudim@gmail.com>
2025-05-22 19:41:17 -0300

    criar conexao

--- src/persistencia/Conexao.java
+++ src/persistencia/Conexao.java
@@ -1,4 +1,31 @@
 package persistencia;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Conexao {
+    private Connection conexao;
+
+    public Connection abrirConexao(){
+        String url="jdbc:mysql://localhost:3306/sistemaacademico?useTimezone=true&serverTimezone=UTC";
+        String user="root";
+        String pass="";
+
+        try {
+            conexao = DriverManager.getConnection(url, user, pass);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return conexao;
+    }
+
+    public void fecharConexao(){
+        try {
+            conexao.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
