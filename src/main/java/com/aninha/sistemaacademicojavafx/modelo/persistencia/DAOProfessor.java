336f8daa4d0613ac78cbcb61d76c0dbfc0001a00
IsraellGaribaldi <164568033+IsraellGaribaldi@users.noreply.github.com>
2025-05-31 20:11:37 -0300

    criação da classe DAOProfessor

---
+++ src/main/java/com/aninha/sistemaacademicojavafx/modelo/persistencia/DAOProfessor.java
@@ -0,0 +1,45 @@
+package com.aninha.sistemaacademicojavafx.modelo.persistencia;
+
+import com.aninha.sistemaacademicojavafx.modelo.Professor;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+
+public class DAOProfessor {
+
+    // Lista que armazena os professores em memória
+    private static ObservableList<Professor> professores = FXCollections.observableArrayList();
+
+    // Código automático para professores
+    private static int proximoCodigoProfessor = 1;
+
+    public DAOProfessor() {
+        // Construtor padrão
+    }
+
+
+    //Lista todos os professores cadastrados.
+    public ObservableList<Professor> listarProfessores() {
+        return professores;
+    }
+
+
+     // Retorna uma cópia da lista de professores (para uso em ComboBox, por exemplo).
+    public ObservableList<Professor> listarProfessoresComboBox() {
+        return FXCollections.observableArrayList(professores);
+    }
+
+
+     // Adiciona um novo professor à lista com código automático.
+    public void adicionar(Professor professor) {
+        professor.setCodigoProfessor(proximoCodigoProfessor++);
+        professores.add(professor);
+    }
+
+
+    // Remove todos os professores e reinicia o contador de código.
+    public void apagarTudo() {
+        professores.clear();
+        proximoCodigoProfessor = 1;
+    }
+}
